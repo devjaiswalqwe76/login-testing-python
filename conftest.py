b6514318b56fa181ac7a25111429d5a5ca291772
Amrit jaiswal <devjaiswalqwe76@gmail.com>
2025-07-18 13:44:22 +0530

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -1,5 +1,16 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture
-def user_info():
-    return {"name": "Simran", "role": "QA Tester"}
+@pytest.fixture(scope="function")
+def driver():
+    # Setup browser
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    yield driver
+    
+    # Teardown
+    driver.quit()
